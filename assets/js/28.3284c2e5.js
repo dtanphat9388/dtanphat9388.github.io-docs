a5b078550f158c0e24b295390860580410e2b748
dtanphat9388 <dtanphat9388@gmail.com>
2018-07-10 13:29:25 +0700

    init

---
+++ assets/js/28.3284c2e5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{60:function(t,n,e){"use strict";e.r(n);var s=e(0),i=Object(s.a)({},function(){var t=this.$createElement;return(this._self._c||t)("img",{attrs:{src:this.$withBase("favicon.ico")}})},[],!1,null,null,null);n.default=i.exports}}]);
